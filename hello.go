63b2848c3c68a56ca1543165f9f445deff2760af
wanbin2014 <wanbin@wans-MacBook-Pro.local>
2014-04-20 17:51:14 +0800

    add newmath invoke

--- hello.go
+++ hello.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+        "fmt"
+        "github.com/wanbin2014/newmath"
+       )
 
 func main() {
-    fmt.Printf("hello world.\n")
+    fmt.Printf("hello world, Sqrt(2)=.\n", newmath.Sqrt(2))
 }
